7123971621be398184bdb8cb55c771ff29bd7a5a
javadbat <javadbat@gmail.com>
2022-04-13 18:18:16 +0430

    add expand row

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import JBGrid from './dist/JBGrid.js';
 import { Cell } from './dist/Components/Cell.js';
 import {Row} from './dist/Components/Row.js';
+import {ExpandRow} from './dist/Components/ExpandRow.js';
 import {JBGridData} from './lib/JBGridData.js';
-export {JBGrid, Row, Cell, JBGridData};
+export {JBGrid, Row, Cell, ExpandRow, JBGridData};
