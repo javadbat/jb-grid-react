5cdd4721f47eaf860a64a3769249da3edb6b4b07
javadbat <javadbat@gmail.com>
2024-02-14 15:23:53 +0330

    fix: Row component accept default div props

--- lib/Components/Row.tsx
+++ lib/Components/Row.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Row.scss';
 import { useJBGridVM } from '../JBGridViewModel';
-function Row(props) {
-    const { children, className} = props;
+type RowProps = Omit<React.ComponentPropsWithoutRef<'div'>,"style">
+function Row(props:RowProps) {
+    const { children, className, ...otherProps} = props;
     const vm = useJBGridVM();
     if(!vm){
         return <React.Fragment></React.Fragment>;
     }
     return (
-        <div className={`jb-grid-table-row ${className??''}`} style={{gridTemplateColumns:vm.styles.table.generalCols.gridTemplateColumns}}>
+        <div className={`jb-grid-table-row ${className??''}`} style={{gridTemplateColumns:vm.styles.table.generalCols.gridTemplateColumns}} {...otherProps}>
             {children}
         </div>
     );
